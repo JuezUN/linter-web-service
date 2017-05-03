d2d276fe66747daefe70999e19475c0203a5ecfe
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-05-02 20:54:45 -0500

    enable clang analysis

--- src/linter-webapp.rb
+++ src/linter-webapp.rb
@@ -1,15 +1,14 @@
 require 'sinatra'
-require 'mkfifo'
 
 post '/cpp' do
   code = params["code"]
-  file_absolute_path = '/home/mauricio/linter-web/codes/code.cpp'
 
+  file_absolute_path = '/home/mauricio/linter-web/codes/code.cpp'
   File.open(file_absolute_path, "w") do |f|
     f.write code
   end
 
-  `/home/mauricio/linters/hw/bin/oclint-0.12 -report-type html #{file_absolute_path} -- -c`
+  `/home/mauricio/linters/hw/bin/oclint-0.12 -report-type html #{file_absolute_path} -- -c -enable-clang-static-analyzer`
 end
 
 post '/*' do
