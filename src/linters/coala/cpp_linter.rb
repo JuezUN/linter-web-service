118964f9a38b8f110f9e4726b4aa2eac36c5ff5c
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-05-29 23:54:40 -0500

    Erase old oclint

--- src/linters/coala/cpp_linter.rb
+++ src/linters/coala/cpp_linter.rb
@@ -4,7 +4,7 @@ require_relative 'json_converter'
 module Coala
   class CppLinter < ::AbstractLinter
     def results
-      json_coala = `coala -I --files #{@file_absolute_path} --bears OCLINTBear --json`
+      json_coala = `coala -I --files #{@file_absolute_path} --bears OclintBear --json`
       Coala::JsonConverter.convert_json_from_coala_to_codemirror(json_coala)
     end
 
